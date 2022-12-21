8689d1f1093d4b3325335186a7536918518584c7
dshah-22 <dhwanishah888@gmail.com>
2022-12-21 20:52:48 +0530

    Initial commit

---
+++ src/main/java/com/paymentgateway/pgexample/Order.java
@@ -0,0 +1,18 @@
+package com.paymentgateway.pgexample;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+
+    private double price;
+    private String currency;
+    private String method;
+    private String intent;
+    private String description;
+
+}
+
---
+++ src/main/java/com/paymentgateway/pgexample/PgexampleApplication.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.pgexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgexampleApplication.class, args);
+	}
+
+}
